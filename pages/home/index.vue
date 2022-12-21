d84f597dbe1bb061633da79e5d794b9f1aef5ba2
bicheng <bicheng.xing@lotusflare.com>
2022-12-21 16:48:04 +0800

    feat: 安装tailwind

---
+++ pages/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container flex itmes-center justify-center">
+        <button onclick="console.log('button has been click')" class="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+            Click me
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { PanelData } from './index.types';
+
+const panelData: PanelData[] = [{
+  title: 'test1'
+}]
+
+console.log(panelData)
+
+export default Vue.extend({
+  name: 'HomePage',
+})
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// 配置tailwind.config.js 内容
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
